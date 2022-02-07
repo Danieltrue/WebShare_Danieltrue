a261c906a4830333cd57eabd18bcf917155ae005
DanielTrue <danielokezie75@gmail.com>
2022-02-07 11:27:18 +0100

    deploy

--- share-it-backend/Server.js
+++ share-it-backend/Server.js
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", router);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "../share-it-app/build")));
+  app.use(express.static(path.join(__dirname, "../share-it-app/build")));
 
   app.get("*", (req, res, next) =>
-    res.sendFile(path.resolve(__dirname, "share-it-app", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "share-it-app", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res, next) => {
